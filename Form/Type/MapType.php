6a0e8c0df52d6d857fda731a08e3cfb1e30502f2
Malte Hübner <maltehuebner@gmx.org>
2018-09-21 08:09:50 +0200

    More work in progress.

--- Form/Type/MapType.php
+++ Form/Type/MapType.php
@@ -6,15 +6,23 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class MapType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        var_dump($options);
+        die;
         $builder
-            ->add('latitude', NumberType::class)
-            ->add('longitude', NumberType::class);
+            ->add('latitude', NumberType::class, [
+                'data' => $options['latitude'],
+            ])
+            ->add('longitude', NumberType::class, [
+                'data' => $options['longitude'],
+            ]);
     }
 
     public function getParent(): string
