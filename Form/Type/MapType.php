6425279dfb6a1fcde0dcc7a78dbc456374b4c0e4
Malte Hübner <maltehuebner@gmx.org>
2018-09-25 17:59:10 +0200

    Work in progress.

--- Form/Type/MapType.php
+++ Form/Type/MapType.php
@@ -5,6 +5,7 @@ namespace Caldera\MapTypeBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
@@ -14,20 +15,18 @@ class MapType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        var_dump($options);
-        die;
-        $builder
-            ->add('latitude', NumberType::class, [
-                'data' => $options['latitude'],
-            ])
-            ->add('longitude', NumberType::class, [
-                'data' => $options['longitude'],
-            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'compound' => false,
+        ]);
     }
 
     public function getParent(): string
     {
-        return FormType::class;
+        return TextType::class;
     }
 
     public function getName(): string
